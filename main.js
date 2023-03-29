66a1967077df06542a5e3b93be1ea7e0a3984dbe
Theresa Ottah <ottahtheresa@gmail.com>
2023-03-30 00:52:30 +0100

    update

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
-
 // Get the question element from the DOM and set its innerHTML to display the generated numbers in a question
 const questionElement = document.getElementById("question");
 questionElement.innerHTML = `What is ${num1} + ${num2} = ?`;
@@ -16,10 +15,8 @@ const inputElement = document.getElementById("input");
 // Get the form element from the DOM
 const formElement = document.getElementById("form");
 
-
 // Get the score element from the DOM
 const scoreElement = document.getElementById("score");
-
 
 //  Initialize the score variable by retrieving it from local storage, or setting it to 0 if it is null
 let score = JSON.parse(localStorage.getItem("score"));
@@ -35,44 +32,39 @@ formElement.addEventListener("submit", (event) => {
   //Prevent the form from submitting and reloading the page
   event.preventDefault();
 
-   // Get the user's answer from the input element
-   const userAnswer = parseInt(inputElement.value);
+  // Get the user's answer from the input element
+  const userAnswer = parseInt(inputElement.value);
 
   // check if the user's answer is correct and update the score accordingly
   if (userAnswer === correctAnswer) {
     score++;
-    localStorageScore(score); // save the score to local storage using the localStorageScore function 
-    generateQuestion(); // generate a new question using the generateQuestion function 
+    localStorageScore(score); // save the score to local storage using the localStorageScore function
+    generateQuestion(); // generate a new question using the generateQuestion function
   } else {
     score--;
     alert(`Wrong! The correct answer is ${correctAnswer}`);
-    localStorageScore(score); // save the score to local storage using the localStorageScore function  
+    localStorageScore(score); // save the score to local storage using the localStorageScore function
   }
 
   // Set the score element's innerHTML to display the score, and clear the input element's value
   scoreElement.innerHTML = `Score: ${score}`;
   inputElement.value = "";
-
 });
-
 
 // Function to update the score in local storage
 function localStorageScore(newScore) {
   localStorage.setItem("score", JSON.stringify(score));
 }
 
-
-// Generate a new question with random numbers and operator using the generateQuestion function 
+// Generate a new question with random numbers and operator using the generateQuestion function
 function generateQuestion() {
   const num1 = Math.ceil(Math.random() * 10);
   const num2 = Math.ceil(Math.random() * 10);
   const operators = ["+", "-", "*", "/"]; //an array of operators
   const operator = operators[Math.floor(Math.random() * operators.length)]; //randomly select an operator from the array
 
-
   // Get the question element from the DOM and set its innerHTML to display the generated numbers in a question
   questionElement.innerHTML = `What is ${num1} ${operator} ${num2} = ?`;
-
 
   // Calculate the correct answer based on the operator
   switch (operator) {
